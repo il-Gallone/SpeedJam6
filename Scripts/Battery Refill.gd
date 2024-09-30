72a35dd1792a5030f1f64d95879601ba185a14c6
Jaffawaffle <drew-of-thassa@hotmail.com>
2024-09-30 23:04:30 +1000

    Adjustments
    level, energy refill

--- Scripts/Battery Refill.gd
+++ Scripts/Battery Refill.gd
@@ -4,7 +4,7 @@ extends "res://Scripts/CollectableBase.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	upgradeAmount = 200
+	upgradeAmount = 150
 	type = upgradeType.REFILL
 	self.add_to_group("Refills")
 	
