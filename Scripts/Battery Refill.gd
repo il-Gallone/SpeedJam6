18703b5365d4155f7db8f2811dc441220538abc4
Jaffawaffle <drew-of-thassa@hotmail.com>
2024-09-30 16:51:06 +1000

    sub 40

--- Scripts/Battery Refill.gd
+++ Scripts/Battery Refill.gd
@@ -4,7 +4,7 @@ extends "res://Scripts/CollectableBase.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	upgradeAmount = 100
+	upgradeAmount = 200
 	type = upgradeType.REFILL
 	self.add_to_group("Refills")
 	
--- Scripts/Speed Part.gd
+++ Scripts/Speed Part.gd
@@ -3,5 +3,5 @@ extends "res://Scripts/CollectableBase.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	upgradeAmount = 20
+	upgradeAmount = 30
 	type = upgradeType.SPEED
